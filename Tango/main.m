83cd0b639e23495cbbe52d30d444759fe15de20b
Lin <lin@LintekiMacBook-Air.local>
2013-10-17 21:54:11 +0800

    Initial Commit

---
+++ Tango/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tango
+//
+//  Created by Lin on 13-10-17.
+//  Copyright (c) 2013年 Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
